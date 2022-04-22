4ee2d72c7b81496a2eddd0123a8f9a9fade73764
Meredith Bailey <meredith.bailey30@gmail.com>
2022-04-22 15:48:27 -0700

    added connection to Firestore and example query

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const {Firestore} = require('@google-cloud/firestore');
+
+// Create a new client
+const firestore = new Firestore();
+
+//Get all docs under the given category
+const get_pet_by_id = async(petId) => {
+    try {
+        let documentRef = firestore.doc('Pets/' + petId);
+        return documentRef.get().then(documentSnapshot => {
+            if (documentSnapshot.exists) {
+                console.log(documentSnapshot.data())
+                return documentSnapshot.data();
+            }
+            else return 'No data retrieved';
+        });
+    } catch(err) {
+        console.log(err);
+    } 
+}
+
 app.use(cors());
 
-app.get('/helloworld', (req, res) => {
-    let data = {data: "This is a message from the server!"}
-    res.send(data)
+app.get('/helloworld', async (req, res) => {
+    let petDocument = await get_pet_by_id('MdYu8EDvl1kcZvSsK9xP');
+    let data = {data: petDocument};
+    res.send(data);
 })
 
 const PORT = process.env.PORT || 8080;
