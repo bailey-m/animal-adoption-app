bfbcf55637bc92b15c3cf7c3c1cd944013ff12ac
Sam Cain <cainsam@oregonstate.edu>
2022-05-01 16:28:26 -0700

    News items work with firebase now. I'm commiting but not pushing in order to impliment future changes by Meredith. I don't think this should create a merge request, but in case it does, please don't merge it.

--- back-end/server.js
+++ back-end/server.js
@@ -5,11 +5,10 @@ const {Firestore, QuerySnapshot} = require('@google-cloud/firestore');
 const firestore = new Firestore();
 app.use(cors());
 
-// *** Begin Pet model functions ***
 
 // Turns a collection into a list of document IDs
 // Based on code from: https://stackoverflow.com/questions/61435004/get-all-documents-from-a-collection
-const get_collection = async(collection) => {
+const get_collection_ids = async(collection) => {
     try {
         let doc_list = [];
         let collectRef = firestore.collection(collection);
@@ -24,9 +23,11 @@ const get_collection = async(collection) => {
     } 
 }
 
+// *** Begin Pet model functions ***
+
 const get_pet_by_id = async(petId) => {
     try {
-        let documentRef = firestore.doc('pets/' + petId);
+        let documentRef = firestore.doc('Pets/' + petId);
         return documentRef.get().then(documentSnapshot => {
             if (documentSnapshot.exists) {
                 return documentSnapshot.data();
@@ -40,6 +41,26 @@ const get_pet_by_id = async(petId) => {
 
 // *** End Pet model functions ***
 
+
+// *** Begin News model functions ***
+
+const get_news_by_id = async(newsId) => {
+    try {
+        let documentRef = firestore.doc('News_Item/' + newsId);
+        return documentRef.get().then(documentSnapshot => {
+            if (documentSnapshot.exists) {
+                return documentSnapshot.data();
+            }
+            else return 'No data retrieved';
+        });
+    } catch(err) {
+        console.log(err);
+    } 
+}
+
+// *** End News model functions ***
+
+
 // *** Begin Pet controller functions ***
 app.get('/helloworld', async (req, res) => {
     let petDocument = await get_pet_by_id('MdYu8EDvl1kcZvSsK9xP');
@@ -49,36 +70,50 @@ app.get('/helloworld', async (req, res) => {
 
 app.get('/pets', async (req, res) => {
     // Get the list of document ids
-    let collection = await get_collection('Pets');
+    let pet_collection = await get_collection_ids('Pets');
     let pets = [];
 
     // Add an entry for each document
-    for (var k in collection){
-        let petDocument = await get_pet_by_id(collection[k]);
-        let data = {data: petDocument};
-        
-        // This for loop only runs once, but it doesn't work without it right now. Can fix later.
-        for(var k in data){
-            temp = {};
-            temp["id"] = data[k].ID;
-            temp["name"] = data[k].Name;
-            temp["image"] = data[k].imageURL;
-            temp["description"] = data[k].Description;
-            temp["age"] = `${data[k].Age} year(s) old`;
-            temp["breed"] = data[k].Breed;
-            temp["availability"] = data[k].Availability;
-            temp["disposition"] = {
-                goodWithAnimals: data[k].Disposition[0],
-                goodWithChildren: data[k].Disposition[1],
-                leashed: data[k].Disposition[2]};
-            temp["species"] = data[k].Species;
-            pets.push(temp);
-        }
-}
+    for (var pet_index in pet_collection){
+        let petDocument = await get_pet_by_id(pet_collection[pet_index]);
+        temp = {};
+        temp["id"] = petDocument.ID;
+        temp["name"] = petDocument.Name;
+        temp["image"] = petDocument.imageURL;
+        temp["description"] = petDocument.Description;
+        temp["age"] = `${petDocument.Age} year(s) old`;
+        temp["breed"] = petDocument.Breed;
+        temp["availability"] = petDocument.Availability;
+        temp["disposition"] = {
+            goodWithAnimals: petDocument.Disposition[0],
+            goodWithChildren: petDocument.Disposition[1],
+            leashed: petDocument.Disposition[2]};
+        temp["species"] = petDocument.Species;
+        pets.push(temp);
+    }
     res.send(pets);
 });
 
 // *** End Pet controller functions ***
+
+app.get('/news', async (req, res) => {
+    // Get the list of document ids
+    let news_collection = await get_collection_ids('News_Item');
+    let news = [];
+
+    // Add an entry for each document
+    for (var news_index in news_collection){
+        let newsDocument = await get_news_by_id(news_collection[news_index]);
+        temp = {};
+        temp["id"] = newsDocument.ID;
+        temp["title"] = newsDocument.Title;
+        temp["image"] = newsDocument.imageURL;
+        temp["description"] = newsDocument.Description;
+        temp["date"] = newsDocument.Date
+        news.push(temp);
+    }
+    res.send(news);
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
